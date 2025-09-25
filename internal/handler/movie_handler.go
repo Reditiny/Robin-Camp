e6498be26b1f8373786c349122b0aa0b11756420
red <erickbin@stu.pku.edu.cn>
2025-09-25 23:07:42 +0800

    models v1

--- internal/handler/movie_handler.go
+++ internal/handler/movie_handler.go
@@ -3,9 +3,12 @@ package handler
 import (
 	"assignment/config"
 	"assignment/internal/client"
+	"assignment/internal/repository"
 	"assignment/openapi"
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	openapi_types "github.com/oapi-codegen/runtime/types"
 )
 
 type MovieHandler struct {
@@ -20,17 +23,63 @@ func NewMovieHandler() *MovieHandler {
 }
 
 func (h *MovieHandler) GetMovies(c *gin.Context, params openapi.GetMoviesParams) {
-	fmt.Println("GetMovies")
-	response, err := h.cli.GetMovieBoxOfficeWithResponse(c, &openapi.GetMovieBoxOfficeParams{Title: "线些西周"})
+
+	movies, nextCursor, err := repository.GetMovies(params)
 	if err != nil {
 		c.JSON(500, gin.H{"error": err.Error()})
 		return
 	}
-	fmt.Println(response)
+
+	data := openapi.MoviePage{Items: make([]openapi.Movie, len(movies))}
+	if nextCursor != "" {
+		data.NextCursor = &nextCursor
+	}
+
+	for i := range movies {
+		data.Items[i] = openapi.Movie{
+			Title:       movies[i].Title,
+			Genre:       movies[i].Genre,
+			ReleaseDate: openapi_types.Date{Time: movies[i].ReleaseDate},
+			Distributor: movies[i].Distributor,
+			Budget:      movies[i].Budget,
+			MpaRating:   movies[i].MpaRating,
+			Id:          movies[i].ID,
+		}
+		response, err := h.cli.GetMovieBoxOfficeWithResponse(c, &openapi.GetMovieBoxOfficeParams{Title: "线些西周"})
+		if err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+		if err = json.Unmarshal(response.Body, &data.Items[i].BoxOffice); err != nil {
+			c.JSON(500, gin.H{"error": err.Error()})
+			return
+		}
+	}
+
+	c.JSON(200, data)
 }
 
 func (h *MovieHandler) PostMovies(c *gin.Context) {
-	fmt.Println("PostMovies")
+	token := c.GetHeader("Authorization")
+
+	if token != "Bearer "+config.Conf.AuthToken {
+		c.JSON(401, gin.H{"error": "Unauthorized"})
+		return
+	}
+
+	m := openapi.Movie{}
+
+	if err := c.BindJSON(&m); err != nil {
+		c.JSON(422, gin.H{"error": err.Error()})
+		return
+	}
+
+	if err := repository.CreateMovie(&m); err != nil {
+		c.JSON(500, gin.H{"error": err.Error()})
+		return
+	}
+
+	c.JSON(201, m)
 }
 
 func (h *MovieHandler) GetMoviesTitleRating(c *gin.Context, title string) {
--- internal/repository/models.go
+++ internal/repository/models.go
@@ -1,12 +1,15 @@
 package repository
 
 import (
+	"assignment/openapi"
+	"fmt"
+	"github.com/google/uuid"
 	"gorm.io/gorm"
 	"time"
 )
 
 type Movie struct {
-	gorm.Model
+	ID          string     `gorm:"primarykey"`
 	Title       string     `gorm:"size:255;not null"`
 	Genre       string     `gorm:"size:255;not null"`
 	ReleaseDate time.Time  `gorm:"not null"`
@@ -18,6 +21,70 @@ type Movie struct {
 
 func (Movie) TableName() string {
 	return "movies"
+}
+
+func CreateMovie(movie *openapi.Movie) error {
+	m := Movie{
+		Title:       movie.Title,
+		Genre:       movie.Genre,
+		ReleaseDate: movie.ReleaseDate.Time,
+		Distributor: movie.Distributor,
+		Budget:      movie.Budget,
+		MpaRating:   movie.MpaRating,
+	}
+
+	m.ID = fmt.Sprintf("m_%s", uuid.New().String())
+	movie.Id = m.ID
+
+	return DB.Create(&m).Error
+}
+
+func GetMovies(params openapi.GetMoviesParams) ([]Movie, string, error) {
+	query := DB.Model(&Movie{})
+
+	if params.Q != nil {
+		query = query.Where("title LIKE ?", "%"+*params.Q+"%")
+	}
+
+	if params.Year != nil {
+		query = query.Where("YEAR(release_date) = ?", *params.Year)
+	}
+
+	if params.Genre != nil {
+		query = query.Where("LOWER(genre) = LOWER(?)", *params.Genre)
+	}
+
+	if params.Distributor != nil {
+		query = query.Where("LOWER(distributor) = LOWER(?)", *params.Distributor)
+	}
+
+	if params.Budget != nil {
+		query = query.Where("budget <= ?", *params.Budget)
+	}
+
+	if params.MpaRating != nil {
+		query = query.Where("mpa_rating = ?", *params.MpaRating)
+	}
+
+	if params.Limit != nil {
+		query = query.Limit(*params.Limit)
+	}
+
+	if params.Cursor != nil {
+		query = query.Where("id > ?", *params.Cursor)
+	}
+
+	var movies []Movie
+	if err := query.Find(&movies).Error; err != nil {
+		return nil, "", err
+	}
+
+	nextCursor := ""
+	if len(movies) > 0 {
+		nextCursor = fmt.Sprintf("%s", movies[len(movies)-1].ID)
+	}
+
+	return movies, nextCursor, nil
 }
 
 type BoxOffice struct {
